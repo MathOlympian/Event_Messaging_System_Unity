75debdb69c1449a2e9a3aa3a92a6a42712fe3d73
my name <myemailid@domain.com>
2018-10-31 06:46:45 +0100

    event test script

---
+++ Scripts/EventTest.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class EventTest : MonoBehaviour
+{
+    private UnityAction someListener;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        someListener = new UnityAction(SomeFunction);
+	}
+
+    private void OnEnable()
+    {
+        EventManager.StartListening("test", someListener);
+    }
+
+    private void OnDisable()
+    {
+        EventManager.StopListening("test", someListener);
+    }
+
+    void SomeFunction()
+    {
+        Debug.Log("SomeFunction called");
+    }
+}
